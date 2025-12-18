5524803baff134241cfec93e0a036de47aebb323
Mateo Ricci Villarruel <mateo.ricci.v@gmail.com>
2025-12-18 01:26:59 -0300

    nueva url

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -6,4 +6,4 @@
 // export const JWT_HOST_API: string = "https://r67lvtpb-3000.brs.devtunnels.ms"; // pc franco
 // export const JWT_HOST_API: string = "https://65j1ms98-3000.brs.devtunnels.ms/"; // notebook franco
 // export const JWT_HOST_API: string = "http://localhost:3000"; // localhost
-export const JWT_HOST_API: string = "https://cec.ginformatico.com.ar/"; // cec
+export const JWT_HOST_API: string = "https://apicec.ginformatico.com.ar/"; // cec
